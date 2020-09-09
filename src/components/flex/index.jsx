3d6296fcf14461c6a6835992be0f0ea4f2f88e86
TserHub <30787150+TserHub@users.noreply.github.com>
2020-09-09 22:38:50 +0800

    --update

--- src/components/flex/index.jsx
+++ src/components/flex/index.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-const items = [1, 2, 3];
 export default function Flex() {
+  const [blocks, setBlocks] = useState([3, 7, 9, 1, 5]);
+
+  const handleChange = (idx) => {
+    setBlocks((prevState) => {
+      return prevState.map((item, index) => {
+        if (index === idx) {
+          return 0;
+        }
+        return item;
+      });
+    });
+  };
+
   return (
     <div className="flex-container">
-      {items.map((item, index) => (
-        <div key={item} className="flex-item">
-          <span className="item-content">{item}</span>
+      {blocks.map((item, index) => (
+        <div key={item} className="flex-item" style={{ order: item }}>
+          <p className="item-content">
+            index:{index}-item:{item}
+          </p>
+          {index === 2 ? (
+            <button onClick={() => handleChange(index)}>Top</button>
+          ) : null}
         </div>
       ))}
     </div>
