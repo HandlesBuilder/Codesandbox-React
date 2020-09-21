9a42212008b506f76767b220daaa9354976d26de
TserHub <30787150+TserHub@users.noreply.github.com>
2020-09-21 20:38:20 +0800

    --update

--- src/components/set-map/index.jsx
+++ src/components/set-map/index.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const s = new Set(["foo", "bar", "foo"]);
+// const m = new Map([["foo", "bar"]]);
 
 export default function SetMap() {
-  const s = new Set(["foo", "bar", "foo"]);
-  for (let item of s.keys()) {
-    console.log(item);
-  }
-  console.log("--");
-  for (let item of s.values()) {
-    console.log(item);
-  }
-  console.log("==");
-  for (let item of s.entries()) {
-    console.log(item);
-  }
+  useEffect(() => {
+    for (let item of s.keys()) {
+      console.log(item);
+    }
+    console.log("--");
+    for (let item of s.values()) {
+      console.log(item);
+    }
+    console.log("==");
+    for (let item of s.entries()) {
+      console.log(item);
+    }
 
-  // Set 结构的实例默认可遍历，它的默认遍历器生成函数就是它的values方法。
-  console.log(Set.prototype[Symbol.iterator] === Set.prototype.values);
-  // 则可以直接用for...of循环遍历 Set。
-  for (let item of s) {
-    console.log(item);
-  }
+    // Set 结构的实例默认可遍历，它的默认遍历器生成函数就是它的values方法。
+    console.log(Set.prototype[Symbol.iterator] === Set.prototype.values);
+    // 则可以直接用for...of循环遍历 Set。
+    for (let item of s) {
+      console.log(item);
+    }
+
+    s.forEach((value, key) => console.log(key + " : " + value));
+  }, []);
+
   return (
     <div>
       <p>Set: {JSON.stringify([...s])}</p>
@@ -47,6 +54,8 @@ export default function SetMap() {
       >
         set -&gt; clear()
       </button>
+      <br />
+      {/* <p>Map: {JSON.stringify(...m)}</p> */}
     </div>
   );
 }
