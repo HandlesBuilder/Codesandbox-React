39f954db58ab45ca7b88c13b352d843e728d5d1d
TserHub <30787150+TserHub@users.noreply.github.com>
2020-10-19 21:24:56 +0800

    --update

---
+++ src/components/grid/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+const arr = new Array(6).fill(0);
+
+function GirdDemo() {
+	return (
+		<div className="grid-container">
+			<div className="grid-wraper">
+				{arr.map((item, index) => (
+					<div key={index} className="grid-item">
+						{index + 1}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default GirdDemo;
