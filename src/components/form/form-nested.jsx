ca79e9e4fb08ac1f57bb5f0cfa2bf10421d05730
TserHub <30787150+TserHub@users.noreply.github.com>
2020-11-13 20:52:32 +0800

    --update

--- src/components/form/form-nested.jsx
+++ src/components/form/form-nested.jsx
@@ -4,8 +4,39 @@ import { Form, Input } from 'antd';
 function FormNested() {
 	return (
 		<Form className="nestedForm">
-			<Form.Item label="foo" name="foo">
-				<Input />
+			<Form.Item
+				label="person"
+				name="person"
+				rules={[
+					{
+						required: true
+					}
+				]}
+			>
+				<Input.Group>
+					<Form.Item
+						name={['person', 'name']}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your name'
+							}
+						]}
+					>
+						<Input />
+					</Form.Item>
+					<Form.Item
+						name={['person', 'age']}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your age'
+							}
+						]}
+					>
+						<Input />
+					</Form.Item>
+				</Input.Group>
 			</Form.Item>
 		</Form>
 	);
