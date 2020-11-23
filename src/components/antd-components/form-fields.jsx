5d714313a78ba39bc0fac19aefff283002439d46
TserHub <30787150+TserHub@users.noreply.github.com>
2020-11-23 20:46:59 +0800

    --update

---
+++ src/components/antd-components/form-fields.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Form, Input } from 'antd';
+
+const CustomizedForm = ({ onChange, fields }) => {
+	return (
+		<Form
+			name="global_state"
+			layout="inline"
+			fields={fields}
+			onFieldsChange={(changedFields, allFields) => {
+				onChange(allFields);
+			}}
+		>
+			<Form.Item
+				name="username"
+				label="Username"
+				rules={[
+					{
+						required: true,
+						message: 'Username is required!'
+					}
+				]}
+			>
+				<Input />
+			</Form.Item>
+		</Form>
+	);
+};
+
+const FormFileds = () => {
+	const [fields, setFields] = useState([
+		{
+			name: ['username'],
+			value: 'Ant Design'
+		}
+	]);
+	return (
+		<>
+			<CustomizedForm
+				fields={fields}
+				onChange={newFields => {
+					setFields(newFields);
+				}}
+			/>
+			<pre style={{ textAlign: 'left' }}>
+				<code>{JSON.stringify(fields, null, 2)}</code>
+			</pre>
+		</>
+	);
+};
+
+export default FormFileds;
--- src/components/antd-components/index.jsx
+++ src/components/antd-components/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TableComp from './table';
 import EmptyComp from './empty';
+import FormFields from './form-fields';
 
 function AntdComponents() {
 	return (
 		<div>
 			<TableComp />
 			<EmptyComp />
+			<FormFields />
 		</div>
 	);
 }
