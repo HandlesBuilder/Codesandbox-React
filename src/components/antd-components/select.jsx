ef75478cb3bb76daf08d0ee5a49be24c94b58ecd
TserHub <jserhub@icloud.com>
2020-11-25 22:07:12 +0800

    --update

--- src/components/antd-components/select.jsx
+++ src/components/antd-components/select.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 import { Select, Pagination } from 'antd';
 
+const data = new Array(100).fill(0).map((item, index) => index + 1);
+
+function getItems(page, pageSize) {
+	const start = (page - 1) * pageSize;
+	return data.slice(start, start + pageSize);
+}
+
 function CustomSelect() {
-	const [items] = useState(['foo', 'bar']);
+	const [items, setItems] = useState(getItems(1, 10));
 	return (
 		<Select
-			style={{ width: 300 }}
+			style={{ width: 500 }}
 			placeholder="custom dropdown render"
 			dropdownRender={menu => (
 				<>
 					{menu}
 					<div style={{ display: 'flex', justifyContent: 'center' }}>
-						<Pagination size="small" defaultCurrent={1} total={50} />
+						<Pagination
+							size="small"
+							defaultCurrent={1}
+							total={data.length}
+							showSizeChanger={false}
+							onChange={(page, pageSize) => {
+								setItems(getItems(page, pageSize));
+							}}
+						/>
 					</div>
 				</>
 			)}
