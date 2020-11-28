fdd496011c3d794a971af776cb163f153ee244c9
TserHub <jserhub@icloud.com>
2020-11-28 14:21:40 +0800

    --update

--- src/components/class-component/index.jsx
+++ src/components/class-component/index.jsx
@@ -1,43 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import SetState from './setState';
+import PropTypes from './proptypes';
 
-class Demo extends Component {
-	state = {
-		num: 0
-	};
-	// 注意setState的第一个参数：字面量和函数
-	handleClick = () => {
-		/* this.setState({ num: this.state.num + 1 }, () => {
-			console.log('callback1:', this.state.num); // 1
-		});
-		console.log('console1', this.state.num); // 0
-		this.setState({ num: this.state.num + 1 }, () => {
-			console.log('callback2:', this.state.num); // 1
-		});
-		console.log('console2', this.state.num); // 0 */
-		this.setState(
-			prevState => ({ num: prevState.num + 1 }),
-			() => {
-				console.log('callback1:', this.state.num); // 2
-			}
-		);
-		console.log('console1', this.state.num); // 0
-		this.setState(
-			prevState => ({ num: prevState.num + 1 }),
-			() => {
-				console.log('callback2:', this.state.num); // 2
-			}
-		);
-		console.log('console2', this.state.num); // 0
-	};
-	render() {
-		const { num } = this.state;
-		return (
-			<div>
-				<p>{num}</p>
-				<button onClick={this.handleClick}>+</button>
-			</div>
-		);
-	}
+function Comp() {
+	return (
+		<>
+			<SetState />
+			<PropTypes />
+		</>
+	);
 }
 
-export default Demo;
+export default Comp;
---
+++ src/components/class-component/proptypes.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// 使用 PropTypes 进行类型检查: https://zh-hans.reactjs.org/docs/typechecking-with-proptypes.html
+class Child extends Component {
+	static propTypes = {
+		str: PropTypes.string.isRequired,
+		num: PropTypes.number.isRequired,
+		enums: PropTypes.oneOf(['foo', 'bar']).isRequired,
+		bool: PropTypes.bool.isRequired,
+		arr: PropTypes.arrayOf(PropTypes.number).isRequired,
+		obj: PropTypes.shape({
+			foo: PropTypes.string
+		}).isRequired,
+		fn: PropTypes.func.isRequired,
+		customProp: function (props, propName, componentName) {
+			if (!/^matchme/.test(props[propName])) {
+				return new Error(
+					'Invalid prop `' +
+						propName +
+						'` supplied to' +
+						' `' +
+						componentName +
+						'`. Validation failed.'
+				);
+			}
+		}
+	};
+	render() {
+		const {
+			str = 'PropsTypes',
+			num = 100,
+			enums = 'bar',
+			bool = false,
+			arr = [],
+			obj = {},
+			fn = () => {},
+			customProp = 'customProp'
+		} = this.props;
+		return (
+			<>
+				<p>{str}</p>
+				<p>{num}</p>
+				<p>{enums}</p>
+				<p>{JSON.stringify(bool)}</p>
+				<p>{JSON.stringify(arr)}</p>
+				<p>{JSON.stringify(obj)}</p>
+				<p>{JSON.stringify(fn)}</p>
+				<p>{customProp}</p>
+			</>
+		);
+	}
+}
+
+class PropTypesComp extends Component {
+	render() {
+		return (
+			<div>
+				<Child
+					str="Hello, string prop!"
+					num={123}
+					enums="foo"
+					bool={false}
+					arr={[1, 2, 3]}
+					obj={{ foo: 'bar' }}
+					fn={() => {
+						console.log('fn');
+					}}
+					customProp="no match me, throw warning"
+				/>
+			</div>
+		);
+	}
+}
+
+export default PropTypesComp;
