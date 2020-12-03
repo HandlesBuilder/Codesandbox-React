a6d8e1bdfc5b23a97c25f6fed9f542dce879bf3c
TserHub <30787150+TserHub@users.noreply.github.com>
2020-12-03 23:23:49 +0800

    --update

--- src/components/array/index.jsx
+++ src/components/array/index.jsx
@@ -8,6 +8,9 @@ const str = `const arrs = [{ foo: 'bar' }, { bar: 'baz' }];
 const arrCopy = arrs.slice(); // 浅拷贝
 arrCopy[0].foo = 'baz';`;
 
+const sliceStr = `Array.prototype.slice.call({ 0: 'a', 1: 'b', length: 2 })`;
+const sliceArr = Array.prototype.slice.call({ 0: 'a', 1: 'b', length: 2 });
+
 export default function ArrayHandler() {
 	const [arr, setArr] = useState([1, 2, 3, 4, 5]);
 	const changeArr = (current, target) => {
@@ -23,15 +26,20 @@ export default function ArrayHandler() {
 			<h3>交换两个数组元素位置</h3>
 			<p>{JSON.stringify(arr)}</p>
 			<button onClick={() => changeArr(1, 3)}>change</button>
+			<pre>
+				<code>{str}</code>
+			</pre>
 			<p>
 				<span>Array.slice()浅拷贝：</span>
 				<span>{JSON.stringify(arrs)}</span>
 				<span>-&gt;</span>
 				<span>{JSON.stringify(arrCopy)}</span>
 			</p>
+			<p>Array.slice() 将类似数组的对象转为真正的数组</p>
 			<pre>
-				<code>{str}</code>
+				<code>{sliceStr}</code>
 			</pre>
+			<p>{JSON.stringify(sliceArr)}</p>
 		</div>
 	);
 }
